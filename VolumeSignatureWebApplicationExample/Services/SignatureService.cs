7ef6b120fd0cf5921b2e9e9c598ee0e673db8708
Maciek <maciek.kaminski.krk@gmail.com>
2022-07-18 22:37:53 +0200

    Comment added

--- VolumeSignatureWebApplicationExample/Services/SignatureService.cs
+++ VolumeSignatureWebApplicationExample/Services/SignatureService.cs
@@ -3,7 +3,15 @@ using System.Text;
 
 namespace VolumeSignatureWebApplicationExample.Services;
 
-public class SignatureService: ISignatureService
+/*
+This service is solely responsible for fetching current public key from Volume domain and verifying 
+if a signature string corresponds to some data. There are two possibilities of passing the data 
+ - as byte array
+ - as a json string
+As the signature in Volumes backend is made by using the json string this is also a recommended method to choose.
+Providing a modified string (or different bytes), other than the one passed in the request body will not pass verification. 
+*/
+public class SignatureService : ISignatureService
 {
     private readonly ILogger<SignatureService> _logger;
     private readonly byte[] _volumePemBytes;
@@ -13,25 +21,26 @@ public class SignatureService: ISignatureService
         _logger = logger;
         _volumePemBytes = GetKeyBytes(configuration["VolumePemUrl"]);
     }
+
     private static byte[] GetKeyBytes(string volumePemUrl)
     {
         var client = new HttpClient();
         client.DefaultRequestHeaders.Add("Accept", "application/json");
         var publicKey = client.GetStringAsync(volumePemUrl);
-        
+
         var keyBytes = Convert.FromBase64String(publicKey.Result);
         return keyBytes;
     }
-    
+
     public bool ValidateSignature(byte[] data, string signatureHeader)
     {
         var r = RSA.Create();
         r.ImportSubjectPublicKeyInfo(_volumePemBytes, out _);
-        
+
         var signatureParts = signatureHeader.Split();
         var expectedSignatureBytes = Convert.FromBase64String(signatureParts[1]);
 
-        return r.VerifyData(data, expectedSignatureBytes, HashAlgorithmName.SHA256  , RSASignaturePadding.Pkcs1);
+        return r.VerifyData(data, expectedSignatureBytes, HashAlgorithmName.SHA256, RSASignaturePadding.Pkcs1);
     }
 
     public bool ValidateSignature(string json, string signatureHeader)
